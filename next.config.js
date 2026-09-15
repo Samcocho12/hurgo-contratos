101109d36fca2590b6d647a502f43290d2c265b5
Jota Dev Soluciones <jorgemarioprado934@gmail.com>
2026-09-14 20:03:16 -0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/assetlinks.json',
+        destination: '/api/assetlinks',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
